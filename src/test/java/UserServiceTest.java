f2b26412bd8cba51e6fc4bf2dbc2d8477817703b
2517594@qq.com <ch1987hzgit>
2016-04-04 08:04:14 +0800

    mybatis单元测试及打印sql

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -1,16 +1,16 @@
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.fly.service.UserService;
 
-public class MybatisTest {
+public class UserServiceTest {
 
-	public static void main(String[] args) {
+	@Test
+	public void count() {
 		ApplicationContext ctx = null;
 		ctx = new ClassPathXmlApplicationContext("spring-mvc.xml");
 		UserService service = (UserService) ctx.getBean("userService");
-		
 		System.out.println(service.count());
 	}
-
 }
